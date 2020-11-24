54acfe464a13d1545bf27ead0509cc7273aab8a3
mathncode <smilekhy@gmail.com>
2020-11-24 21:39:04 +0900

    User 엔티티 관련 코드 작성

---
+++ src/main/java/org/example/springboot/domain/user/Role.java
@@ -0,0 +1,15 @@
+package org.example.springboot.domain.user;
+// 사용자 권한 관리 클래스
+
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
+@Getter
+@RequiredArgsConstructor
+public enum Role {
+
+    GUEST("ROLE_GUEST", "손님"), USER("ROLE_USER", "일반 사용자");
+
+    private final String key;
+    private final String title;
+}
